a054f13edb289f51bc7136ac71b421830ad7a991
MrIanis <ianis.pouru@gmail.com>
2023-05-04 17:49:02 +0200

    Supression CHECK

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,24 +1,20 @@
 import { useCallback, useEffect, useState } from "react";
-import { useParams } from "react-router";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-} from "@material-ui/core";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "./Note.css";
 
 const Note = ({ onSave, onDelete }) => {
   const { id } = useParams();
   const [note, setNote] = useState(null);
-  const [showConfirmationDialog, setShowConfirmationDialog] = useState(false);
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const navigate = useNavigate();
 
   const fetchNote = useCallback(async () => {
-    const response = await fetch("/notes/${id}");
+    const response = await fetch(`/notes/${id}`);
     const result = await response.json();
     setNote(result);
+
   }, [id]);
 
   useEffect(() => {
@@ -27,7 +23,7 @@ const Note = ({ onSave, onDelete }) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    await fetch("/notes/${id}", {
+    await fetch(`/notes/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -37,63 +33,69 @@ const Note = ({ onSave, onDelete }) => {
     onSave();
   };
 
-  const handleDelete = async (event) => {
-    event.preventDefault();
-    setShowConfirmationDialog(true);
-  };
-
-  const handleDeleteConfirmed = async () => {
-    setShowConfirmationDialog(false);
-    await fetch("/notes/${id}", {
+  const handleDelete = async () => {
+    
+    await fetch(`/notes/${id}`, {
       method: "DELETE",
     });
-    onDelete();
+    onSave();
+    navigate("/");
+    
+    
   };
 
-  const handleDeleteCancelled = () => {
-    setShowConfirmationDialog(false);
+  const handleShowDeleteConfirmation = () => {
+    setShowDeleteConfirmation(true);
+  };
+
+  const handleHideDeleteConfirmation = () => {
+    setShowDeleteConfirmation(false);
   };
 
   return (
-    <form className="Form" onSubmit={handleSubmit}>
-      <input
-        className="Note-editable Note-title"
-        type="text"
-        value={note ? note.title : ""}
-        onChange={(event) => {
-          setNote({ ...note, title: event.target.value });
-        }}
-      />
-      <textarea
-        className="Note-editable Note-content"
-        value={note ? note.content : ""}
-        onChange={(event) => {
-          setNote({ ...note, content: event.target.value });
-        }}
-      />
-      <div className="Note-actions ">
-        <button className="Button" type="submit">
-          Enregistrer
-        </button>
-        <button className="Button" onClick={handleDelete}>
-          Supprimer
-        </button>
-      </div>
-      <Dialog open={showConfirmationDialog}>
-        <DialogTitle>Confirmation</DialogTitle>
-        <DialogContent>
-          Êtes-vous sûr de vouloir supprimer cette note ?
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleDeleteConfirmed} color="primary">
-            Oui
-          </Button>
-          <Button onClick={handleDeleteCancelled} color="primary" autoFocus>
-            Non
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </form>
+    <>
+      <form className="Form" onSubmit={handleSubmit}>
+        <input
+          className="Note-editable Note-title"
+          type="text"
+          value={note ? note.title : ""}
+          onChange={(event) => {
+            setNote({ ...note, title: event.target.value });
+          }}
+        />
+        <textarea
+          className="Note-editable Note-content"
+          value={note ? note.content : ""}
+          onChange={(event) => {
+            setNote({ ...note, content: event.target.value });
+          }}
+        />
+        <div className="Note-actions ">
+          <button className="Button" type="submit">
+            Enregistrer
+          </button>
+          <button className="Button" onClick={handleShowDeleteConfirmation}>
+            Supprimer
+          </button>
+        </div>
+      </form>
+      {showDeleteConfirmation && (
+        <div className="Overlay">
+          <div className="Confirmation">
+            <p>Êtes-vous sûr de vouloir supprimer cette note ?</p>
+            <div>
+              <button className="Button" onClick={handleDelete}>
+                Oui
+                
+              </button>
+              <button className="Button" onClick={handleHideDeleteConfirmation}>
+                Non
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
